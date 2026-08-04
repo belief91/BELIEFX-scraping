bd3607447a002398f3947e93d19e48e96565117b
Belief <finoanarafanom2991@gmail.com>
2026-08-04 11:31:34 +0300

    Renforcement headers anti-blocage TV5MONDE

--- index.js
+++ index.js
@@ -47,6 +47,10 @@ const HEADERS = {
   "User-Agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
     "(KHTML, like Gecko) Chrome/124.0 Safari/537.36",
+  "Accept":
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+  "Accept-Language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+  "Referer": "https://www.google.com/",
 };
 
 const DATE_CLASS_PATTERN = /^\d{4}-\d{2}-\d{2}$/;
